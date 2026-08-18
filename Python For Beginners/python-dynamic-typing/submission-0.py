0bc97619dda9e4ef0e304c7892c3adb356a6b2d5
Joey Kim <joeykim999@gmail.com>
2026-08-18 17:46:18 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 4
+print(type(variable))
+variable = 3.3
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "this"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
