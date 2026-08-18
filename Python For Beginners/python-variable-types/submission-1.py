f111656c2e9f3faeb4669661299f57371ace9317
Joey Kim <joeykim999@gmail.com>
2026-08-18 17:44:01 -0500

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,11 @@
+integer_type = 3
+float_type = 3.3
+bool_type = True
+string_type = "hi"
+list_type = [1,3,2]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
