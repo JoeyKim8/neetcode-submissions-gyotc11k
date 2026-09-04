058073d021429fbb012949c3d92b6be9dd9b1ff3
Joey Kim <joeykim999@gmail.com>
2026-09-03 23:37:44 -0500

    Add: python-dict-remove - submission-2

---
+++ Python For Beginners/python-dict-remove/submission-2.py
@@ -0,0 +1,13 @@
+from typing import Dict, List
+
+def remove_keys(my_dict: Dict[str, int], keys: List[str]) -> Dict[str, int]:
+    for k in keys:  # iterate over every key in the list of keys
+        my_dict.pop(k, 0) # check that the key exists in my_dict, if not return 0, if it does exist then pop (remove) it
+    return my_dict
+
+
+
+
+# do not modify below this line
+print(remove_keys({"a": 1, "b": 2, "c": 3}, ["a", "c"]))
+print(remove_keys({"a": 1, "b": 2, "c": 3}, ["d"]))
