5615af8bed89077498f358d2c4eac8edcb930669
Joey Kim <joeykim999@gmail.com>
2026-09-06 17:19:24 -0500

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,16 @@
+def add_two_numbers() -> int:
+    user_input = input() # take input
+    string_list = user_input.split(",") # split the number input by commas
+    # the numbers are still strings rn
+
+    sum = int(string_list[0]) + int(string_list[1])
+    return sum
+
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
